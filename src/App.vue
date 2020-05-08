344aaf00d09aa1d469e95d0b53865ec7a3ac2612
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2020-05-07 21:40:08 -0500

    todo list ready

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>ToDo List with Vue</h1>
+    <div class="container">
+      <div class="inputItem">
+        <input type="text" v-model="message">
+        <button v-on:click="addToDo">➕</button>
+      </div>
+      <ol>
+        <li v-for="todo in todos" v-bind:key="todo.id">
+          <span>{{ todo.id + '. ' + todo.text }}</span>
+          <div class="editItem">
+            <button v-on:click="updateItem(todo.id)">⭕</button>
+            <button v-on:click="deleteItem(todo.id)">❌</button>
+          </div>
+        </li>
+      </ol>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './assets/sass/app.sass'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    const app = {
+      message: '',
+      todos: [
+      ]
+    }
+    return app;
+  },
+  methods:{
+    addToDo(){
+      if (this.message) {
+        this.todos.push({ id: this.todos.length, text: this.message });
+        this.message = '';
+      }
+    },
+    deleteItem(id){
+      this.todos = this.todos.filter(e => e.id != id)
+    },
+    updateItem(id){
+      const idx = this.todos.findIndex(e => e.id == id)
+      this.todos[idx].text = prompt('ingresa un nuevo texto', this.todos[idx].text)
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+
 </style>
--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,12 +1,30 @@
 <template>
-  <li>{{ todo.text }}</li>
+  <li>
+    {{ todo.text }}
+    <button v-on:click="deleteItem">&times;</button>
+  </li>
 </template>
+
+// ! LLAMADA
+<!-- <ol>
+  <ToDoItem 
+    v-for="todo in todos"
+    v-bind:todo="todo"
+    v-bind:key="todo.id">
+  </ToDoItem>
+</ol> -->
+// import ToDoItem from './components/ToDoItem';
 
 <script>
 export default {
-  name: 'ToDoList',
+  name: 'ToDoItem',
   props: {
     todo: Object
+  },
+  methods:{
+    deleteItem(){
+      console.log(this.todo.id);
+    }
   }
 }
 </script>
